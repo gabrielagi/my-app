86ab9f1b7ceae90e963f1a715489043a4ca32ada
Gabriela Iriart <gabriela.iriart@devlabs.dev>
2025-05-21 21:51:20 -0300

    mi primer componente

---
+++ components/banner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <>
+      <h1 className="bg-indigo-400">Banner</h1>
+      <p>Mi primer componente</p>
+    </>
+  );
+};
+
+export default Banner;
